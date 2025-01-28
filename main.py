ea408823b9bf4d3918f4b065f758724e69d9c365
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-01-28 17:54:51 -0600

    need d

--- main.py
+++ main.py
@@ -6,7 +6,26 @@ q = 115578717622022981
 
 n = p * q
 f = (p - 1) * (q - 1)
-e = 65537
+e = 3 
 d = 218457
 print((e * d) % f)
-print(d)
+print(math.gcd(f,e))
+
+
+sample = 12345
+
+
+def encrypt(plain_text, public_key):
+    encrypted = pow(plain_text, public_key, n)
+    return encrypted
+
+
+def decrypt(encrypted_message, private_key):
+    decrypted = pow(encrypted_message, private_key, n)
+    return decrypted
+
+
+enc_m = encrypt(sample, e)
+dec_m = decrypt(enc_m, d)
+
+print(dec_m)
